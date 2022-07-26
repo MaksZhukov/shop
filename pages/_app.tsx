1b1ceb390b5d044ed12d273c17855846355a4976
Maksim Zhukau <maksimz@checkpoint.com>
2022-07-26 17:57:04 +0300

    Added chat-bot

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,86 +1,86 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import { Provider } from 'mobx-react';
-import { store } from '../store';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import Content from '../components/Content';
-import Layout from '../components/Layout';
-import { createTheme, Snackbar, Stack, ThemeProvider } from '@mui/material';
-import { green, lightGreen, lime, red, grey } from '@mui/material/colors';
-import { useEffect } from 'react';
+import "../styles/globals.css";
+import TawkTo from "tawkto-react";
+import type { AppProps } from "next/app";
+import { Provider } from "mobx-react";
+import { store } from "../store";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Content from "../components/Content";
+import Layout from "../components/Layout";
+import { createTheme, Snackbar, Stack, ThemeProvider } from "@mui/material";
+import { green, lightGreen, lime, red, grey } from "@mui/material/colors";
+import { useEffect } from "react";
 import {
-    getCartProductIDs,
-    getFavoriteProductIDs,
-    getJwt,
-    getReviewEmail,
-    saveJwt
-} from '../services/LocalStorageService';
-import Notification from '../components/Notification';
-import RouteShield from '../components/RouteShield/RouteShield';
-import { fetchProducts } from 'api/products/products';
-import { Favorite } from 'api/favorites/types';
-import { ShoppingCartItem } from 'api/cart/types';
+  getJwt,
+  getReviewEmail,
+  saveJwt,
+} from "../services/LocalStorageService";
+import Notification from "../components/Notification";
+import RouteShield from "../components/RouteShield/RouteShield";
+
+const propertyId = "62d12b117b967b117999a7b7";
+const tawkId = "1g80h0foo";
 
 let theme = createTheme({
-    typography: {
-        fontFamily: 'Lato, Arial'
+  typography: {
+    fontFamily: "Lato, Arial",
+  },
+  palette: {
+    primary: {
+      ...green,
+      contrastText: "#fff",
     },
-    palette: {
-        primary: {
-            ...green,
-            contrastText: '#fff'
-        }
-    }
+  },
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
-    useEffect(() => {
-        const tryFetchData = async () => {
-            let token = getJwt();
-            if (token) {
-                store.user.setJWT(token);
-                try {
-                    await Promise.all([
-                        store.user.loadInfo(),
-                        store.cart.loadShoppingCart(),
-                        store.favorites.loadFavorites()
-                    ]);
-                } catch (err) {
-                    saveJwt('');
-                }
-            } else {
-                const email = getReviewEmail();
+  useEffect(() => {
+    const tryFetchData = async () => {
+      let token = getJwt();
+      if (token) {
+        store.user.setJWT(token);
+        try {
+          await Promise.all([
+            store.user.loadInfo(),
+            store.cart.loadShoppingCart(),
+            store.favorites.loadFavorites(),
+          ]);
+        } catch (err) {
+          saveJwt("");
+        }
+      } else {
+        const email = getReviewEmail();
 
-                const [cartItems, favoriteItems, data] = await Promise.all([
-                    store.cart.getShoppingCartByLocalStorage(),
-                    store.favorites.getFavoritesByLocalStorage(),
-                    ...(email ? [store.user.loadReviewStatus(email)] : [])
-                ]);
-                store.cart.setCartItems(cartItems);
-                store.favorites.setFavorites(favoriteItems);
-            }
-            store.setIsInitialRequestDone();
-        };
-        tryFetchData();
-    }, []);
+        const [cartItems, favoriteItems, data] = await Promise.all([
+          store.cart.getShoppingCartByLocalStorage(),
+          store.favorites.getFavoritesByLocalStorage(),
+          ...(email ? [store.user.loadReviewStatus(email)] : []),
+        ]);
+        store.cart.setCartItems(cartItems);
+        store.favorites.setFavorites(favoriteItems);
+      }
+      store.setIsInitialRequestDone();
+    };
+    tryFetchData();
+    new TawkTo(propertyId, tawkId);
+  }, []);
 
-    return (
-        <ThemeProvider theme={theme}>
-            <Provider store={store}>
-                <Layout>
-                    <Header></Header>
-                    <Content>
-                        <RouteShield>
-                            <Component {...pageProps} />
-                        </RouteShield>
-                    </Content>
-                    <Footer></Footer>
-                    <Notification></Notification>
-                </Layout>
-            </Provider>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <Layout>
+          <Header></Header>
+          <Content>
+            <RouteShield>
+              <Component {...pageProps} />
+            </RouteShield>
+          </Content>
+          <Footer></Footer>
+          <Notification></Notification>
+        </Layout>
+      </Provider>
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
