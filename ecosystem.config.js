ff2fe0e707879ffa352bda0347f5a7f5d2f67ad5
MaksZhukov <maks_zhukov_97@mail.ru>
2022-08-20 17:10:44 +0300

    Added pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+	apps: [
+		// {
+		//     name: "app1",
+		//     script: "./server.js",
+		//     env: {
+		//         PORT: 1337,
+		//         NODE_ENV: "production",
+		//     },
+		// },
+		// {
+		//     name: "app2",
+		//     script: "./server.js",
+		//     env: {
+		//         PORT: 1338,
+		//         NODE_ENV: "production",
+		//     },
+		// },
+		{
+			script: 'server.js',
+			name: 'client',
+			env: {
+				NODE_ENV: 'production',
+			},
+			// instances: "max",
+			// exec_mode: "cluster",
+			max_memort_restart: '500m',
+		},
+	],
+};
