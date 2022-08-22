136e1cd8bd10f0eb8ac4f2e16ea5d779b903b627
MaksZhukov <maks_zhukov_97@mail.ru>
2022-08-22 19:34:28 +0300

    Filled about us page

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,25 +3,27 @@ import { Container } from '@mui/system';
 import styles from './Footer.module.scss';
 
 const Footer = () => {
-    let date = new Date();
-    return (
-        <footer className={styles.footer}>
-            <Container>
-                <Typography variant="h6" component="h6">
-                    ЦЕНТР ОБСЛУЖИВАНИЯ КЛИЕНТОВ: +375-29-780-4-780, +375-29-601-16-02, +375-29-888-32-33
-                </Typography>
-                <Typography margin="0.5em 0" component="p">
-                    ООО &quot;Дриблинг&quot;, 2009-2017. УНП 590740644 . Дата регистрации 03.11.2008.
-                </Typography>
-                <Typography margin="0.5em 0" component="p">
-                    Адрес: Республика Беларусь, Гродненская область, Гродненский район. д. Полотково
-                </Typography>
-                <Typography margin="0.5em 0" component="p">
-                    © {date.getFullYear()} - Авторазборка Полотково
-                </Typography>
-            </Container>
-        </footer>
-    );
+	let date = new Date();
+	return (
+		<footer className={styles.footer}>
+			<Container>
+				<Typography variant='h6' component='h6'>
+					ЦЕНТР ОБСЛУЖИВАНИЯ КЛИЕНТОВ: +375-29-780-47-80
+				</Typography>
+				<Typography margin='0.5em 0' component='p'>
+					ООО &quot;Дриблинг&quot;, 2009-2017. УНП 590740644 . Дата
+					регистрации 03.11.2008.
+				</Typography>
+				<Typography margin='0.5em 0' component='p'>
+					Адрес: Республика Беларусь, Гродненская область, Гродненский
+					район. д. Полотково
+				</Typography>
+				<Typography margin='0.5em 0' component='p'>
+					© {date.getFullYear()} - Авторазборка Полотково
+				</Typography>
+			</Container>
+		</footer>
+	);
 };
 
 export default Footer;
--- pages/contacts.tsx
+++ pages/contacts.tsx
@@ -11,6 +11,7 @@ import { addReview } from 'api/reviews/reviews';
 import Loader from 'components/Loader';
 import WhiteBox from 'components/WhiteBox';
 import { observer } from 'mobx-react-lite';
+import Image from 'next/image';
 import { ChangeEvent, FormEvent, FormEventHandler, useState } from 'react';
 import { saveReviewEmail } from 'services/LocalStorageService';
 import { useStore } from 'store';
@@ -19,7 +20,37 @@ import styles from './contacts.module.scss';
 const Contacts = () => {
 	return (
 		<Container>
-			<WhiteBox>Contacts</WhiteBox>
+			<WhiteBox>
+				<Typography
+					marginBottom='0.5em'
+					component='h1'
+					variant='h4'
+					textAlign='center'>
+					О нас
+				</Typography>
+				<Typography textAlign='center' variant='h6'>
+					Время работы:
+				</Typography>
+				<Typography
+					textAlign='center'
+					marginLeft='2em'
+					variant='subtitle1'>
+					Понедельник - Пятница: 10:00-18:00
+				</Typography>
+				<Typography
+					textAlign='center'
+					marginLeft='2em'
+					variant='subtitle1'>
+					Суббота - Воскресенье: 10:00-14:00
+				</Typography>
+				<Box textAlign='center'>
+					<Image
+						alt='Путь'
+						width={390}
+						height={685}
+						src={'/route.png'}></Image>
+				</Box>
+			</WhiteBox>
 		</Container>
 	);
 };
