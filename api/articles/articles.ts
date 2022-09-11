c2ccf3996b168390c79e48140147455791cf4cfe
MaksZhukov <maks_zhukov_97@mail.ru>
2022-09-11 15:07:52 +0300

    Fixed getting models by brand

--- api/articles/articles.ts
+++ api/articles/articles.ts
@@ -1,5 +1,16 @@
 import { api } from '..';
-import { CollectionParams } from 'api/types';
+import { ApiResponse, CollectionParams } from 'api/types';
+import { Article } from './types';
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig } = getConfig();
 
 export const getArticles = (params: CollectionParams) =>
-	api.get('/articles', { params });
+	api.get<ApiResponse<Article[]>>('/articles', { params });
+
+export const fetchArticle = (idOrSlug: string, isServerRequest = false) =>
+	api.get<ApiResponse<Article>>(`/articles/${idOrSlug}`, {
+		...(isServerRequest
+			? { baseURL: publicRuntimeConfig.backendLocalUrl + '/api' }
+			: {}),
+	});
