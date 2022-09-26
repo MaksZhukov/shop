3e532d60abf827dfe420c60b217c45f062fae0e0
MaksZhukov <maks_zhukov_97@mail.ru>
2022-09-26 23:53:25 +0300

    Added preload for fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,18 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-    return (
-        <Html>
-            <Head>
-                <link
-                    rel="stylesheet"
-                    href="https://cdnjs.cloudflare.com/ajax/libs/lato-font/3.0.0/css/lato-font.min.css"
-                />
-            </Head>
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+	return (
+		<Html>
+			<Head>
+				<link
+					rel='preload'
+					href='https://cdnjs.cloudflare.com/ajax/libs/lato-font/3.0.0/css/lato-font.min.css'
+				/>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
