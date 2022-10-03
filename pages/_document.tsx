265b3caf9d7e5350d6904cd088e9315d1245a3f0
MaksZhukov <maks_zhukov_97@mail.ru>
2022-10-03 20:22:44 +0300

    Added lang attr

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
 	return (
-		<Html>
+		<Html lang='ru'>
 			<Head>
 				<link
 					rel='stylesheet preload'
