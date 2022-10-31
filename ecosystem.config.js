0580776d0b6cd440d67cd55e2084e70ce83ce270
Maksim Zhukau <maksimz@checkpoint.com>
2022-10-31 08:40:14 +0200

    Removed test file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,30 +1,32 @@
 module.exports = {
-	apps: [
-		// {
-		//     name: "app1",
-		//     script: "./server.js",
-		//     env: {
-		//         PORT: 1337,
-		//         NODE_ENV: "production",
-		//     },
-		// },
-		// {
-		//     name: "app2",
-		//     script: "./server.js",
-		//     env: {
-		//         PORT: 1338,
-		//         NODE_ENV: "production",
-		//     },
-		// },
-		{
-			script: 'server.js',
-			name: 'client',
-			env: {
-				NODE_ENV: 'production',
-			},
-			// instances: "max",
-			// exec_mode: "cluster",
-			max_memort_restart: '500m',
-		},
-	],
+  apps: [
+    // {
+    //     name: "app1",
+    //     script: "./server.js",
+    //     env: {
+    //         PORT: 1337,
+    //         NODE_ENV: "production",
+    //     },
+    // },
+    // {
+    //     name: "app2",
+    //     script: "./server.js",
+    //     env: {
+    //         PORT: 1338,
+    //         NODE_ENV: "production",
+    //     },
+    // },
+    {
+      script: "server.js",
+      name: "client",
+      env: {
+        PORT: 3000,
+        NODE_ENV: "production",
+        NODE_TLS_REJECT_UNAUTHORIZED: 0,
+      },
+      // instances: "max",
+      // exec_mode: "cluster",
+      max_memort_restart: "500m",
+    },
+  ],
 };
