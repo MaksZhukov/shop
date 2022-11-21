25b33a3f819c4ebd1c6a8af5a5705d27fbddfb34
MaksZhukov <maks_zhukov_97@mail.ru>
2022-11-21 23:22:43 +0300

    Fixed Seo box

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,31 +1,31 @@
 module.exports = {
-  apps: [
-    // {
-    //     name: "app1",
-    //     script: "./server.js",
-    //     env: {
-    //         PORT: 1337,
-    //         NODE_ENV: "production",
-    //     },
-    // },
-    // {
-    //     name: "app2",
-    //     script: "./server.js",
-    //     env: {
-    //         PORT: 1338,
-    //         NODE_ENV: "production",
-    //     },
-    // },
-    {
-      script: "server.js",
-      name: "client",
-      env: {
-        PORT: 3000,
-        NODE_ENV: "production"
-      },
-      instances: "max",
-      exec_mode: "cluster",
-      max_memory_restart: "500M",
-    },
-  ],
+	apps: [
+		// {
+		//     name: "app1",
+		//     script: "./server.js",
+		//     env: {
+		//         PORT: 1337,
+		//         NODE_ENV: "production",
+		//     },
+		// },
+		// {
+		//     name: "app2",
+		//     script: "./server.js",
+		//     env: {
+		//         PORT: 1338,
+		//         NODE_ENV: "production",
+		//     },
+		// },
+		{
+			script: 'server.js',
+			name: 'client',
+			env: {
+				PORT: 3000,
+				NODE_ENV: 'production',
+			},
+			instances: 'max',
+			exec_mode: 'cluster',
+			max_memory_restart: '500M',
+		},
+	],
 };
