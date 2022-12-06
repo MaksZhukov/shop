0f952672e052bcd2560e4a9e02843b51286e22d8
MaksZhukov <maks_zhukov_97@mail.ru>
2022-12-06 23:02:30 +0300

    Added revalidation for all pages

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,18 +1,35 @@
-import { NextApiResponse, NextApiRequest } from "next";
+import { NextApiResponse, NextApiRequest } from 'next';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.query.revalidateToken !== process.env.REVALIDATE_TOKEN) {
-    return res.status(401).json({ message: "Invalid token" });
-  }
-  try {
-    if (typeof req.query.pagePath === "string") {
-      await res.revalidate(req.query.pagePath);
-    }
-    return res.json({ revalidated: true });
-  } catch (err) {
-    return res.status(500).send("Error revalidating");
-  }
+const PAGES = [
+	'/404',
+	'/500',
+	'/autocomises',
+	'/car-dismantling-photos',
+	'/vacancies',
+	'/service-stations',
+	'/profile',
+	'/installment-plan',
+	'/how-to-get-to',
+	'/guarantee',
+	'/favorites',
+	'/delivery',
+	'/contacts',
+];
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	if (req.query.revalidateToken !== process.env.REVALIDATE_TOKEN) {
+		return res.status(401).json({ message: 'Invalid token' });
+	}
+	try {
+		if (typeof req.query.pagePath === 'string') {
+			if (req.query.pagePath === '*') {
+				await Promise.all(PAGES.map((page) => res.revalidate(page)));
+			} else {
+				await res.revalidate(req.query.pagePath);
+			}
+		}
+		return res.json({ revalidated: true });
+	} catch (err) {
+		return res.status(500).send('Error revalidating');
+	}
 }
--- pages/autocomises.tsx
+++ pages/autocomises.tsx
@@ -2,63 +2,58 @@ import { Box, Link, Typography } from '@mui/material';
 import { Container } from '@mui/system';
 import { ApiResponse, MetaResponse } from 'api/types';
 import HeadSEO from 'components/HeadSEO';
-import Image from 'components/Image';
 import SEOBox from 'components/SEOBox';
 import WhiteBox from 'components/WhiteBox';
 import { NextPage } from 'next';
-import getConfig from 'next/config';
 import { getPageProps } from 'services/PagePropsService';
-import NextLink from 'next/link';
 import { fetchAutocomises } from 'api/autocomises/autocomises';
 import { Autocomis } from 'api/autocomises/types';
 import { fetchPage } from 'api/pages';
 import { DefaultPage } from 'api/pages/types';
 import Card from 'components/Card';
 
-const { publicRuntimeConfig } = getConfig();
-
 interface Props {
-    page: DefaultPage;
-    autocomises: ApiResponse<Autocomis[]>;
+	page: DefaultPage;
+	autocomises: ApiResponse<Autocomis[]>;
 }
 
 const Vacancies: NextPage<Props> = ({ page, autocomises }) => {
-    return (
-        <Container>
-            <HeadSEO
-                title={page.seo?.title}
-                description={page.seo?.description}
-                keywords={page.seo?.keywords}
-            ></HeadSEO>
+	return (
+		<Container>
+			<HeadSEO
+				title={page.seo?.title}
+				description={page.seo?.description}
+				keywords={page.seo?.keywords}
+			></HeadSEO>
 
-            <WhiteBox>
-                <Typography textAlign="center" component="h1" variant="h4" marginBottom="1em">
-                    {page.seo?.h1 || 'Автокомисы'}
-                </Typography>
-                {autocomises.data.map((item) => (
-                    <Card
-                        key={item.id}
-                        description={item.description}
-                        name={item.name}
-                        image={item.image}
-                        link={`/autocomises/${item.slug}`}
-                    ></Card>
-                ))}
-            </WhiteBox>
-            <SEOBox images={page.seo?.images} content={page.seo?.content}></SEOBox>
-        </Container>
-    );
+			<WhiteBox>
+				<Typography textAlign='center' component='h1' variant='h4' marginBottom='1em'>
+					{page.seo?.h1 || 'Автокомисы'}
+				</Typography>
+				{autocomises.data.map((item) => (
+					<Card
+						key={item.id}
+						description={item.description}
+						name={item.name}
+						image={item.image}
+						link={`/autocomises/${item.slug}`}
+					></Card>
+				))}
+			</WhiteBox>
+			<SEOBox images={page.seo?.images} content={page.seo?.content}></SEOBox>
+		</Container>
+	);
 };
 
 export default Vacancies;
 
 export const getStaticProps = getPageProps(fetchPage('autocomis'), async () => ({
-    autocomises: (
-        await fetchAutocomises(
-            {
-                populate: 'image',
-            },
-            true
-        )
-    ).data,
+	autocomises: (
+		await fetchAutocomises(
+			{
+				populate: 'image',
+			},
+			true
+		)
+	).data,
 }));
