e5754040712b56b6bb066b6e55b004f826594771
Maksim Zhukau <maksimz@checkpoint.com>
2022-12-19 13:58:27 +0300

    Added new static pages and added car on parts page

---
+++ pages/payment.tsx
@@ -0,0 +1,21 @@
+import WhiteBox from 'components/WhiteBox';
+import { getPageProps } from 'services/PagePropsService';
+import { fetchPage } from 'api/pages';
+import { DefaultPage } from 'api/pages/types';
+import ReactMarkdown from 'components/ReactMarkdown';
+
+interface Props {
+	page: DefaultPage & { content: string };
+}
+
+const Contacts = ({ page }: Props) => {
+	return (
+		<WhiteBox>
+			<ReactMarkdown content={page.content}></ReactMarkdown>
+		</WhiteBox>
+	);
+};
+
+export default Contacts;
+
+export const getStaticProps = getPageProps(fetchPage('payment'));
