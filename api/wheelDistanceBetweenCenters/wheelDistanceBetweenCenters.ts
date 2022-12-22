eda4ef198c9b2eaafca4b4f3b29e9b25c876db93
MaksZhukov <maks_zhukov_97@mail.ru>
2022-12-22 23:44:02 +0300

    Removed distance between centers as relation

--- api/wheelDistanceBetweenCenters/wheelDistanceBetweenCenters.ts
+++
@@ -1,6 +0,0 @@
-import { api } from '..';
-import { ApiResponse, CollectionParams } from '../types';
-import { WheelDistanceBetweenCenter } from './types';
-
-export const fetchWheelDistanceBetweenCenters = (params?: CollectionParams) =>
-	api.get<ApiResponse<WheelDistanceBetweenCenter[]>>('/wheel-distance-between-centers', { params });
