b90ffadb2d5c5993f7f6145a5035604cbae479fc
MaksZhukov <maks_zhukov_97@mail.ru>
2023-01-09 23:02:36 +0300

    Added list view for reviews

--- pages/reviews.tsx
+++ pages/reviews.tsx
@@ -1,21 +1,15 @@
-import { Button, Divider, Link, Pagination, PaginationItem, Rating, Typography, useMediaQuery } from '@mui/material';
+import { Button, Divider, Link, Pagination, PaginationItem, Rating, useMediaQuery } from '@mui/material';
 import { Box } from '@mui/system';
 import { fetchPage } from 'api/pages';
 import { DefaultPage } from 'api/pages/types';
 import { fetchReviews } from 'api/reviews/reviews';
 import { Review } from 'api/reviews/types';
-import AddReview from 'components/pages/reviews/AddReview';
+import Typography from 'components/Typography';
 import WhiteBox from 'components/WhiteBox';
 import { observer } from 'mobx-react-lite';
-import { useRouter } from 'next/router';
 import { useSnackbar } from 'notistack';
 import { Fragment, useEffect, useState } from 'react';
 import { getPageProps } from 'services/PagePropsService';
-import { useStore } from 'store';
-import NextLink from 'next/link';
-import CarouselReviews from 'components/CarouselReviews';
-
-let COUNT_REVIEWS = 10;
 
 interface Props {
 	page: DefaultPage;
@@ -23,8 +17,6 @@ interface Props {
 
 const Reviews = ({ page }: Props) => {
 	const [reviews, setReviews] = useState<Review[]>([]);
-	const isTablet = useMediaQuery((theme: any) => theme.breakpoints.down('md'));
-	const isMobile = useMediaQuery((theme: any) => theme.breakpoints.down('sm'));
 
 	const { enqueueSnackbar } = useSnackbar();
 
@@ -44,18 +36,44 @@ const Reviews = ({ page }: Props) => {
 		fetchData();
 	}, []);
 
+	const renderReviews = reviews.map((item, index) => (
+		<Fragment key={item.id}>
+			<Box marginY='0.5em' key={item.id}>
+				<Typography title={item.authorName} lineClamp={1} component='legend'>
+					{item.authorName}
+				</Typography>
+				<Rating readOnly value={item.rating}></Rating>
+				<Typography title={item.description} lineClamp={2} variant='body1'>
+					{item.description}
+				</Typography>
+			</Box>
+			{index !== reviews.length - 1 && <Divider></Divider>}
+		</Fragment>
+	));
+
 	return (
-		<Box>
+		<WhiteBox>
 			<Typography gutterBottom component='h1' variant='h4' textAlign='center'>
 				{page.seo?.h1 || 'Отзывы'}
 			</Typography>
-			<CarouselReviews reviews={reviews} slidesToShow={isMobile ? 1 : isTablet ? 3 : 5}></CarouselReviews>
-
-			{/* <AddReview></AddReview> */}
-		</Box>
+			{renderReviews}
+			<Typography marginRight='1em' display='inline' variant='h6'>
+				<Link
+					target='_blank'
+					href='https://www.google.com/maps/place/%D0%A0%D0%B0%D0%B7%D0%B1%D0%BE%D1%80%D0%BA%D0%B0+%D0%9F%D0%BE%D0%BB%D0%BE%D1%82%D0%BA%D0%BE%D0%B2%D0%BE+%D0%BC%D0%B0%D0%B3%D0%B0%D0%B7%D0%B8%D0%BD+%D0%B7%D0%B0%D0%BF%D1%87%D0%B0%D1%81%D1%82%D0%B5%D0%B9+%D0%B1%D1%83+%D0%B4%D0%BB%D1%8F+%D0%B0%D0%B2%D1%82%D0%BE/@53.5848407,23.8611008,15z/data=!4m7!3m6!1s0x0:0xc75df7b81d42a898!8m2!3d53.5848407!4d23.8611008!9m1!1b1'
+				>
+					Посмотреть все отзывы
+				</Link>
+			</Typography>
+			<Typography display='inline' variant='h6'>
+				<Link href='https://g.page/r/CZioQh24913HEB0/review' target='_blank'>
+					Оставить отзыв
+				</Link>
+			</Typography>
+		</WhiteBox>
 	);
 };
 
-export default observer(Reviews);
+export default Reviews;
 
 export const getStaticProps = getPageProps(fetchPage('review'));
