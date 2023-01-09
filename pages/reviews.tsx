e1f204135dc9f1ff40e2634e16d40ee6c6b9b762
MaksZhukov <maks_zhukov_97@mail.ru>
2023-01-09 11:15:27 +0300

    Added reviews

--- pages/reviews.tsx
+++ pages/reviews.tsx
@@ -1,4 +1,4 @@
-import { Button, Divider, Link, Pagination, PaginationItem, Rating, Typography } from '@mui/material';
+import { Button, Divider, Link, Pagination, PaginationItem, Rating, Typography, useMediaQuery } from '@mui/material';
 import { Box } from '@mui/system';
 import { fetchPage } from 'api/pages';
 import { DefaultPage } from 'api/pages/types';
@@ -13,68 +13,46 @@ import { Fragment, useEffect, useState } from 'react';
 import { getPageProps } from 'services/PagePropsService';
 import { useStore } from 'store';
 import NextLink from 'next/link';
+import CarouselReviews from 'components/CarouselReviews';
 
 let COUNT_REVIEWS = 10;
 
 interface Props {
-	data: DefaultPage;
+	page: DefaultPage;
 }
 
-const Reviews = ({ data }: Props) => {
+const Reviews = ({ page }: Props) => {
 	const [reviews, setReviews] = useState<Review[]>([]);
+	const isTablet = useMediaQuery((theme: any) => theme.breakpoints.down('md'));
+	const isMobile = useMediaQuery((theme: any) => theme.breakpoints.down('sm'));
 
-	const router = useRouter();
-	const store = useStore();
 	const { enqueueSnackbar } = useSnackbar();
 
 	const fetchData = async () => {
-		const {
-			data: { data },
-		} = await fetchReviews();
-		setReviews(data);
+		try {
+			const {
+				data: { data },
+			} = await fetchReviews();
+			setReviews(data);
+		} catch (err) {
+			enqueueSnackbar('Произошла какая-то ошибка с загрузкой отзывов, обратитесь в поддержку', {
+				variant: 'error',
+			});
+		}
 	};
 	useEffect(() => {
 		fetchData();
 	}, []);
 
 	return (
-		<WhiteBox>
-			<Typography component='h1' variant='h4' textAlign='center'>
-				{data.seo?.h1 || 'Отзывы'}
+		<Box>
+			<Typography gutterBottom component='h1' variant='h4' textAlign='center'>
+				{page.seo?.h1 || 'Отзывы'}
 			</Typography>
-			{reviews.map((item, index) => (
-				<Fragment key={item.id}>
-					<Box paddingY='1em' key={item.id}>
-						<Typography color='text.secondary'>{item.authorName}</Typography>
-						<Rating readOnly value={item.rating}></Rating>
-						{/* <Typography color='text.secondary'>
-							{new Date(item.publishedAt).toLocaleDateString()}
-						</Typography> */}
-						{item.description && (
-							<Typography marginTop='0.5em' color='text.secondary'>
-								{item.description}
-							</Typography>
-						)}
-					</Box>
-					{reviews.length - 1 !== index && <Divider></Divider>}
-				</Fragment>
-			))}
-			<Typography variant='h6'>
-				<Link href='https://g.page/r/CZioQh24913HEB0/review' target='_blank'>
-					Оставить отзыв
-				</Link>
-			</Typography>
-			<Typography variant='h6'>
-				<Link
-					target='_blank'
-					href='https://www.google.com/maps/place/%D0%A0%D0%B0%D0%B7%D0%B1%D0%BE%D1%80%D0%BA%D0%B0+%D0%9F%D0%BE%D0%BB%D0%BE%D1%82%D0%BA%D0%BE%D0%B2%D0%BE+%D0%BC%D0%B0%D0%B3%D0%B0%D0%B7%D0%B8%D0%BD+%D0%B7%D0%B0%D0%BF%D1%87%D0%B0%D1%81%D1%82%D0%B5%D0%B9+%D0%B1%D1%83+%D0%B4%D0%BB%D1%8F+%D0%B0%D0%B2%D1%82%D0%BE/@53.5848407,23.8611008,15z/data=!4m7!3m6!1s0x0:0xc75df7b81d42a898!8m2!3d53.5848407!4d23.8611008!9m1!1b1'
-				>
-					Посмотреть все отзывы
-				</Link>
-			</Typography>
+			<CarouselReviews reviews={reviews} slidesToShow={isMobile ? 1 : isTablet ? 3 : 5}></CarouselReviews>
 
 			{/* <AddReview></AddReview> */}
-		</WhiteBox>
+		</Box>
 	);
 };
 
