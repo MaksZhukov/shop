40354fd2b4d14e9c6a24aefe2ed1d44d59c9938c
MaksZhukov <maks_zhukov_97@mail.ru>
2023-01-10 17:11:12 +0300

    Added head meta for socials

--- components/HeadSEO/HeadSEO.tsx
+++ components/HeadSEO/HeadSEO.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { FC } from 'react';
 
 interface Props {
@@ -7,11 +8,21 @@ interface Props {
 	keywords?: string;
 }
 
+const origin = typeof window !== 'undefined' && window.location.origin ? window.location.origin : '';
+
 const HeadSEO: FC<Props> = ({ title, description, keywords }) => {
+	const router = useRouter();
 	return (
 		<Head>
-			<title>{title}</title> <meta name='description' content={description}></meta>
+			<title>{title}</title>
+			<meta name='description' content={description}></meta>
 			<meta name='keywords' content={keywords} />
+			<meta property='og:url' content={origin + router.asPath} />
+			<meta property='og:site_name' content='Разбор авто' />
+			<meta property='og:title' content={title} />
+			<meta property='og:description' content={description} />
+			<meta property='twitter:title' content={title} />
+			<meta property='twitter:description' content={description} />
 		</Head>
 	);
 };
