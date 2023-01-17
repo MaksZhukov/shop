2ad1a1ba658500e77de94dfc565e797725e3910a
Maksim Zhukau <maksimz@checkpoint.com>
2023-01-17 10:24:19 +0300

    Removed reviews adding functionality

--- api/reviews/reviews.ts
+++ api/reviews/reviews.ts
@@ -1,15 +1,6 @@
-import { ApiRounded } from '@mui/icons-material';
 import { api } from 'api';
 import { ApiResponse, CollectionParams } from 'api/types';
-import { ReviewStatus } from 'store/User';
 import { Review } from './types';
 
-export const addReview = (data: Omit<Review, 'id' | 'publishedAt'>) =>
-	api.post<ApiResponse<Review>>('/reviews', { data });
 
 export const fetchReviews = (params?: CollectionParams) => api.get<ApiResponse<Review[]>>('/reviews', { params });
-
-export const checkReviewStatus = (email: string) =>
-	api.get<ApiResponse<{ status: ReviewStatus }>>('/reviews/check', {
-		params: { email },
-	});
--- services/LocalStorageService.ts
+++ services/LocalStorageService.ts
@@ -1,4 +1,3 @@
-import { Favorite } from 'api/favorites/types';
 import { ProductType } from 'api/types';
 
 export const saveJwt = (jwt: string) => {
@@ -30,48 +29,6 @@ export const removeFavoriteProduct = (id: number, type: ProductType) => {
 	let favoriteProducts = getFavoriteProducts();
 	localStorage.setItem(
 		'favoriteProducts',
-		JSON.stringify(
-			favoriteProducts.filter(
-				(item) => item.id !== id && item.type === type
-			)
-		)
+		JSON.stringify(favoriteProducts.filter((item) => item.id !== id && item.type === type))
 	);
-};
-
-// export const getCartProducts = (): ProductLocalStorage[] => {
-// 	let result = localStorage.getItem('cartProducts');
-// 	return result ? JSON.parse(result) : [];
-// };
-
-// export const saveCartProduct = (id: number, type: ProductType) => {
-// 	let cartProducts = getCartProducts();
-// 	cartProducts.push({ id, type, createdAt: new Date().getTime() });
-// 	localStorage.setItem('cartProducts', JSON.stringify(cartProducts));
-// };
-
-// export const removeCartProduct = (id: number, type: ProductType) => {
-// 	let cartProducts = getCartProducts();
-// 	localStorage.setItem(
-// 		'cartProducts',
-// 		JSON.stringify(
-// 			cartProducts.filter((item) => item.id !== id && item.type === type)
-// 		)
-// 	);
-// };
-
-export const saveIsReviewAdded = (value: boolean) => {
-	localStorage.setItem('isReviewAdded', `${value}`);
-};
-
-export const getIsReviewAdded = (): boolean => {
-	let result = localStorage.getItem('isReviewAdded');
-	return result ? JSON.parse(result) : false;
-};
-
-export const saveReviewEmail = (email: string) => {
-	localStorage.setItem('reviewEmail', email);
-};
-
-export const getReviewEmail = (): string => {
-	return localStorage.getItem('reviewEmail') || '';
 };
