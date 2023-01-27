14cf288eda63aa2f3efe1a59fde84650bd4d9ef2
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-01-28 00:34:14 +0300

    Added redesign for articles

--- components/CardItem/CardItem.tsx
+++ components/CardItem/CardItem.tsx
@@ -7,49 +7,53 @@ import { FC } from 'react';
 import Typography from 'components/Typography';
 import ReactMarkdown from 'components/ReactMarkdown';
 interface Props {
-	image?: IImage;
-	description: string;
-	name: string;
-	link: string;
+    image?: IImage;
+    description: string;
+    name: string;
+    link: string;
 }
 
 const CardItem: FC<Props> = ({ image, description, name, link }) => {
-	const isMobile = useMediaQuery((theme: any) => theme.breakpoints.down('sm'));
-	return (
-		<Box display='flex' marginBottom='2em' sx={{ flexDirection: { xs: 'column', sm: 'row' } }}>
-			<Image
-				alt={image?.alternativeText || name}
-				width={isMobile ? 500 : 208}
-				height={isMobile ? 375 : 156}
-				src={
-					isMobile
-						? image?.formats?.small.url || image?.url || ''
-						: image?.formats?.thumbnail.url || image?.url || ''
-				}
-				style={isMobile ? { height: 'auto' } : {}}
-			></Image>
+    const isMobile = useMediaQuery((theme: any) => theme.breakpoints.down('sm'));
+    return (
+        <Box display="flex" marginBottom="3em" sx={{ flexDirection: { xs: 'column', sm: 'row' } }}>
+            <Image
+                alt={image?.alternativeText || name}
+                width={isMobile ? 500 : 330}
+                height={isMobile ? 375 : 220}
+                src={
+                    isMobile
+                        ? image?.formats?.small.url || image?.url || ''
+                        : image?.formats?.thumbnail.url || image?.url || ''
+                }
+                style={isMobile ? { height: 'auto' } : {}}></Image>
 
-			<Box sx={{ marginLeft: { xs: '0', sm: '1em' }, marginTop: { xs: '1em', sm: '0' } }}>
-				<Typography component='h2' variant='h5' gutterBottom>
-					<NextLink href={link}>
-						<Link component='span' underline='hover'>
-							{name}
-						</Link>
-					</NextLink>
-				</Typography>
-				<Box>
-					<ReactMarkdown inline content={`${description?.substring(0, 500)}...`}></ReactMarkdown>
-					<Typography component='span'>
-						<NextLink href={link}>
-							<Link component='span' underline='hover'>
-								читать далее
-							</Link>
-						</NextLink>
-					</Typography>
-				</Box>
-			</Box>
-		</Box>
-	);
+            <Box sx={{ marginLeft: { xs: '0', sm: '1em' }, marginTop: { xs: '1em', sm: '0' } }}>
+                <Typography component="h2" variant="h5" gutterBottom>
+                    <NextLink href={link}>
+                        <Link
+                            component="span"
+                            color="secondary.main"
+                            textTransform="uppercase"
+                            fontWeight="500"
+                            underline="hover">
+                            {name}
+                        </Link>
+                    </NextLink>
+                </Typography>
+                <Box>
+                    <ReactMarkdown inline content={`${description?.substring(0, 500)}...`}></ReactMarkdown>
+                    <Typography marginTop="0.5em">
+                        <NextLink href={link}>
+                            <Link color="text.secondary" component="span" underline="hover">
+                                Читать далее >>
+                            </Link>
+                        </NextLink>
+                    </Typography>
+                </Box>
+            </Box>
+        </Box>
+    );
 };
 
 export default CardItem;
--- components/ReactMarkdown/ReactMarkdown.tsx
+++ components/ReactMarkdown/ReactMarkdown.tsx
@@ -12,63 +12,60 @@ const ReactPlayer = dynamic(() => import('react-player'), { ssr: false });
 const { publicRuntimeConfig } = getConfig();
 
 interface Props {
-	content: string;
-	inline?: boolean;
+    content: string;
+    inline?: boolean;
 }
 
 const ReactMarkdown: FC<Props> = ({ content, inline }) => {
-	const isMobile = useMediaQuery((theme: any) => theme.breakpoints.down('sm'));
-	return (
-		<ReactMarkdownLib
-			rehypePlugins={[rehypeRaw]}
-			components={{
-				img: ({ src, alt = '' }) => {
-					return (
-						<Image
-							alt={alt}
-							width={isMobile ? 500 : 640}
-							height={isMobile ? 375 : 480}
-							src={src || ''}
-							{...(isMobile ? { style: { height: 'auto' } } : {})}
-						></Image>
-					);
-				},
-				video: ({ src }) => {
-					return (
-						<ReactPlayer
-							width={isMobile ? '100%' : 640}
-							height={isMobile ? 'auto' : 480}
-							controls
-							style={{ margin: '1em' }}
-							url={publicRuntimeConfig.backendLocalUrl + src}
-						></ReactPlayer>
-					);
-				},
-				p: (data) => {
-					return (
-						<Typography display={inline ? 'inline' : 'block'} gutterBottom>
-							{data.children}
-						</Typography>
-					);
-				},
-				a: (data) => {
-					return <Link href={data.href}>{data.children}</Link>;
-				},
-				h2: (data) => (
-					<Typography component='h2' gutterBottom marginTop='1em' variant='h5'>
-						{data.children}
-					</Typography>
-				),
-				h1: (data) => (
-					<Typography component='h1' textAlign='center' variant='h4'>
-						{data.children}
-					</Typography>
-				),
-			}}
-		>
-			{content}
-		</ReactMarkdownLib>
-	);
+    const isMobile = useMediaQuery((theme: any) => theme.breakpoints.down('sm'));
+    return (
+        <ReactMarkdownLib
+            rehypePlugins={[rehypeRaw]}
+            components={{
+                img: ({ src, alt = '' }) => {
+                    return (
+                        <Image
+                            alt={alt}
+                            width={isMobile ? 500 : 640}
+                            height={isMobile ? 375 : 480}
+                            src={src || ''}
+                            {...(isMobile ? { style: { height: 'auto' } } : {})}></Image>
+                    );
+                },
+                video: ({ src }) => {
+                    return (
+                        <ReactPlayer
+                            width={isMobile ? '100%' : 640}
+                            height={isMobile ? 'auto' : 480}
+                            controls
+                            style={{ margin: '1em' }}
+                            url={publicRuntimeConfig.backendLocalUrl + src}></ReactPlayer>
+                    );
+                },
+                p: (data) => {
+                    return (
+                        <Typography display={inline ? 'inline' : 'block'} gutterBottom>
+                            {data.children}
+                        </Typography>
+                    );
+                },
+                a: (data) => {
+                    return <Link href={data.href}>{data.children}</Link>;
+                },
+                h2: (data) => (
+                    <Typography component="h2" gutterBottom marginTop="1em" variant="h5">
+                        {data.children}
+                    </Typography>
+                ),
+                h1: (data) => (
+                    <Typography component="h1" textAlign="center" variant="h4">
+                        {data.children}
+                    </Typography>
+                )
+            }}>
+            {content}
+        </ReactMarkdownLib>
+    );
 };
 
 export default ReactMarkdown;
