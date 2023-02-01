2c0ba9d6673915b6a2b308907ef231671451c9d1
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-02-02 00:29:00 +0300

    Started redesing delivery page

--- pages/delivery.tsx
+++ pages/delivery.tsx
@@ -1,26 +1,73 @@
 import { FC } from 'react';
-import { Typography } from '@mui/material';
-import WhiteBox from 'components/WhiteBox';
+
 import { getPageProps } from 'services/PagePropsService';
 import { fetchPage } from 'api/pages';
 import { DefaultPage } from 'api/pages/types';
 import ReactMarkdown from 'components/ReactMarkdown';
+import Image from 'components/Image';
+import { Box, Container } from '@mui/system';
+import Typography from 'components/Typography';
 
 interface Props {
-	page: DefaultPage & { content: string };
+    page: DefaultPage & { content: string };
 }
 
-const InstallmentPlan: FC<Props> = ({ page }) => {
-	return (
-		<WhiteBox>
-			<Typography gutterBottom component='h1' variant='h4' textAlign='center'>
-				{page.seo?.h1 || "Доставка"}
-			</Typography>
-			<ReactMarkdown content={page.content}></ReactMarkdown>
-		</WhiteBox>
-	);
+const Delivery: FC<Props> = ({ page }) => {
+    return (
+        <>
+            <Container>
+                <Box display="flex">
+                    <Image width={500} height={360} src="/delivery_img.png" isOnSSR={false} alt={'Delivery'}></Image>
+                    <Box>
+                        <Typography variant="h3" fontWeight="500" marginBottom="1em" component="h1">
+                            Доставка запчастей бу
+                        </Typography>
+                        <Typography>
+                            Если Вы видите знак на карточке товара [ *Бесплатная доставка ] – данный товар может быть
+                            доставлен в ближайшее отделение ОПС ЕвроПочты за сумму указанную в карточке товара, без
+                            оплаты стоимости доставки! Для заказа используйте кнопку “Быстрый заказ”.
+                        </Typography>
+                    </Box>
+                </Box>
+            </Container>
+            <Box bgcolor="#fff">
+                <Container>
+                    <Box display="flex">
+                        <Box>
+                            <Image
+                                width={500}
+                                height={360}
+                                src="/delivery_img.png"
+                                isOnSSR={false}
+                                alt={'Delivery'}></Image>
+                        </Box>
+                        <Box>
+                            <Image
+                                width={500}
+                                height={360}
+                                src="/delivery_img.png"
+                                isOnSSR={false}
+                                alt={'Delivery'}></Image>
+                        </Box>
+                        <Box>
+                            {' '}
+                            <Image
+                                width={500}
+                                height={360}
+                                src="/delivery_img.png"
+                                isOnSSR={false}
+                                alt={'Delivery'}></Image>
+                        </Box>
+                    </Box>
+                </Container>
+            </Box>
+            <Box>
+                <Typography withSeparator>Доставка в другие города (по Беларуси)</Typography>
+            </Box>
+        </>
+    );
 };
 
-export default InstallmentPlan;
+export default Delivery;
 
-export const getStaticProps = getPageProps(fetchPage('delivery'));
+export const getStaticProps = getPageProps(fetchPage('delivery'), () => ({ hasGlobalContainer: false }));
