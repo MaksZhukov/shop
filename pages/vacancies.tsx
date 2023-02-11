8aca0dc141f6ee29ff64fcfde9d8600ae06c647d
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-02-11 19:50:50 +0300

    Added page vacancies dynamic

--- pages/vacancies.tsx
+++ pages/vacancies.tsx
@@ -1,85 +1,23 @@
 import { Table, TableBody, TableCell, TableRow } from '@mui/material';
-import { ApiResponse } from 'api/types';
-import WhiteBox from 'components/WhiteBox';
 import { NextPage } from 'next';
 import { getPageProps } from 'services/PagePropsService';
-import { fetchVacancies } from 'api/vacancies/vacancies';
-import { Vacancy } from 'api/vacancies/types';
 import { fetchPage } from 'api/pages';
-import { DefaultPage } from 'api/pages/types';
-import CardItem from 'components/CardItem';
-import { Box, Container } from '@mui/system';
+import { PageVacancies, Vacancy } from 'api/pages/types';
+import { Box } from '@mui/system';
 import Image from 'components/Image';
 import Typography from 'components/Typography';
 import BlockImages from 'components/BlockImages';
 
 interface Props {
-    page: DefaultPage;
-    vacancies: ApiResponse<Vacancy[]>;
+    page: PageVacancies;
 }
 
-const VACANCIES = [
-    {
-        id: 1,
-        images: ['/vacancy_1.png', '/vacancy_1.png', '/vacancy_1.png', '/vacancy_1.png'],
-        title: 'Вакансии в магазин запчастей в Гродно',
-        vacancy: 'продавец-консультант по подбору',
-        description: [
-            { label: 'Форма занятости:', value: 'Постоянная' },
-            { label: 'График работы:', value: 'Полный' },
-            { label: 'Образование:', value: 'Высшее' },
-            { label: 'Тип работодателя:', value: 'Компания' }
-        ],
-        fullTitle:
-            'Приглашаем на работу в магазин автозапчастей для автотехники продавца-консультанта (подборщика автозапчастей).',
-        responsibilities: [
-            'обработка заявок клиентов на подбор автозапчастей, консультирование клиентов',
-            'помощь и контроль при оприходовании и отпуске запасных частей со склада',
-            'ведение отчетности'
-        ],
-        requirenments: [
-            'профильное образование (автослесарь, автомеханик, автотракторный факультет и др.)',
-            'знание современных моделей грузовой автотехники',
-            'опыт работы автомехаником, мастером, автослесарем',
-            'уверенный пользователь ПК (опыт работы с автокатологами)',
-            'общительность, позитивность, ответственность, исполнительность'
-        ]
-    },
-    {
-        id: 2,
-        images: ['/vacancy_2.png', '/vacancy_2.png', '/vacancy_2.png', '/vacancy_2.png'],
-        title: 'Вакансии в магазин запчастей в Гродно',
-        vacancy: 'Требуется продавец в автомагазин',
-        description: [
-            { label: 'Форма занятости:', value: 'Постоянная' },
-            { label: 'График работы:', value: 'Полный' },
-            { label: 'Образование:', value: 'Высшее' },
-            { label: 'Тип работодателя:', value: 'Компания' }
-        ],
-        fullTitle:
-            'Приглашаем на работу в магазин автозапчастей для автотехники продавца-консультанта (подборщика автозапчастей).',
-        responsibilities: [
-            'обработка заявок клиентов на подбор автозапчастей, консультирование клиентов',
-            'помощь и контроль при оприходовании и отпуске запасных частей со склада',
-            'ведение отчетности'
-        ],
-        requirenments: [
-            'знание компьютера',
-            'умение вести бухгалтерский учет',
-            'наличие прав на управление транспортными средствами категории "В"',
-            'наличие опыта в сфере торговли',
-            'общительность, позитивность, ответственность, исполнительность'
-        ]
-    }
-];
-
-const Vacancies: NextPage<Props> = ({ page, vacancies }) => {
-    let renderVacancy = (item: typeof VACANCIES[0], index: number) => {
-        let [mainImg, ...otherImgs] = item.images;
+const Vacancies: NextPage<Props> = ({ page }) => {
+    let renderVacancy = (item: Vacancy, index: number) => {
         return (
             <>
                 <Box key={item.id} display="flex">
-                    <Image isOnSSR={false} src={mainImg} alt="alt" width={500} height={360}></Image>
+                    <Image src={item.image?.url} alt={item.image.alternativeText} width={500} height={360}></Image>
                     <Box paddingLeft="3em">
                         <Typography
                             marginBottom="1em"
@@ -111,7 +49,7 @@ const Vacancies: NextPage<Props> = ({ page, vacancies }) => {
                         </Table>
                     </Box>
                 </Box>
-                <BlockImages imageSRCs={otherImgs}></BlockImages>
+                <BlockImages images={item.images}></BlockImages>
                 <Typography
                     marginBottom="2em"
                     component="h2"
@@ -127,7 +65,7 @@ const Vacancies: NextPage<Props> = ({ page, vacancies }) => {
                             Обязаности
                         </Typography>
                         <Box component="ul" padding="0">
-                            {item.responsibilities.map((val) => (
+                            {item.responsibilities.split('\n').map((val) => (
                                 <Typography marginBottom="1em" key={val} component="li">
                                     {val}
                                 </Typography>
@@ -142,7 +80,7 @@ const Vacancies: NextPage<Props> = ({ page, vacancies }) => {
                             Требования
                         </Typography>
                         <Box component="ul" padding="0">
-                            {item.requirenments.map((val) => (
+                            {item.requirements.split('\n').map((val) => (
                                 <Typography marginBottom="1em" key={val} component="li">
                                     {val}
                                 </Typography>
@@ -153,21 +91,11 @@ const Vacancies: NextPage<Props> = ({ page, vacancies }) => {
             </>
         );
     };
-    return <>{VACANCIES.map(renderVacancy)}</>;
+    return <>{page.vacancies.map(renderVacancy)}</>;
 };
 
 export default Vacancies;
 
 export const getStaticProps = getPageProps(
-    fetchPage('vacancy'),
-    async () => ({
-        vacancies: (
-            await fetchVacancies({
-                populate: 'image'
-            })
-        ).data
-    }),
-    () => ({
-        hideSEOBox: true
-    })
+    fetchPage('vacancy', { populate: ['vacancies.image', 'vacancies.images', 'seo', 'vacancies.description'] })
 );
