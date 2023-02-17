ee5dbd6571d51a5b77b5114c5b24acdc1c9ea502
Maksim Zhukau <maksimz@checkpoint.com>
2023-02-17 20:10:51 +0300

    Added delivery mobile page

--- pages/delivery.tsx
+++ pages/delivery.tsx
@@ -7,103 +7,134 @@ import ReactMarkdown from 'components/ReactMarkdown';
 import Image from 'components/Image';
 import { Box, Container } from '@mui/system';
 import Typography from 'components/Typography';
-import { Table, TableBody, TableCell, TableRow } from '@mui/material';
+import { Table, TableBody, TableCell, TableRow, SxProps, useMediaQuery } from '@mui/material';
 import BlockImages from 'components/BlockImages';
 
 interface Props {
-    page: PageDelivery;
+	page: PageDelivery;
 }
 
 const Delivery: FC<Props> = ({ page }) => {
-    return (
-        <>
-            <Box marginBottom="4em" display="flex">
-                <Image
-                    title={page.mainImageLeft?.caption}
-                    width={page.mainImageLeft?.width}
-                    height={page.mainImageLeft?.height}
-                    src={page.mainImageLeft?.url}
-                    alt={page.mainImageLeft?.alternativeText}></Image>
-                <Box marginLeft="2.5em">
-                    <Typography variant="h3" fontWeight="500" marginBottom="1em" component="h1">
-                        {page.h1}
-                    </Typography>
-                    <Typography>
-                        <ReactMarkdown content={page.mainTextRight}></ReactMarkdown>
-                    </Typography>
-                </Box>
-            </Box>
-            <BlockImages images={page.images1}></BlockImages>
-            <Typography marginTop="1em" textTransform="uppercase" withSeparator component="h2" variant="h4">
-                {page.deliveryCitiesTitle}
-            </Typography>
-            <Table sx={{ marginY: '2em', maxWidth: 930 }}>
-                <TableBody>
-                    {page.deliveryCitiesDescription.map((item) => (
-                        <TableRow key={item.value}>
-                            <TableCell width={600}>
-                                <Typography whiteSpace="nowrap">{item.label}</Typography>
-                            </TableCell>
-                            <TableCell>
-                                <Typography fontWeight="500">{item.value}</Typography>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
+	const isTablet = useMediaQuery((theme: any) => theme.breakpoints.down('md'));
+	const renderH1 = (sx: SxProps) => (
+		<Box sx={sx} fontWeight='500' textTransform='uppercase' marginBottom='1em' component='h1'>
+			{page.h1}
+		</Box>
+	);
+	return (
+		<>
+			<Box
+				display='flex'
+				sx={{ marginBottom: { xs: '1em', md: '4em' }, flexDirection: { xs: 'column', md: 'row' } }}
+			>
+				{renderH1({ display: { xs: 'block', md: 'none' }, typography: 'h4', marginBottom: '1em' })}
+				<Image
+					title={page.mainImageLeft?.caption}
+					width={page.mainImageLeft?.width}
+					height={page.mainImageLeft?.height}
+					src={page.mainImageLeft?.url}
+					alt={page.mainImageLeft?.alternativeText}
+					style={isTablet ? { height: 'auto' } : {}}
+				></Image>
+				<Box sx={{ marginLeft: { xs: 0, md: '2.5em' }, marginTop: { xs: '1em', md: 0 } }}>
+					{renderH1({ display: { xs: 'none', md: 'block' }, typography: 'h3' })}
+					<Typography>
+						<ReactMarkdown content={page.mainTextRight}></ReactMarkdown>
+					</Typography>
+				</Box>
+			</Box>
+			<BlockImages
+				withoutOverlay={isTablet}
+				sx={{ marginY: '0', paddingY: 0 }}
+				images={page.images1}
+			></BlockImages>
+			<Typography marginTop='1em' textTransform='uppercase' withSeparator component='h2' variant='h4'>
+				{page.deliveryCitiesTitle}
+			</Typography>
+			<Table sx={{ marginY: '2em', maxWidth: 930 }}>
+				<TableBody>
+					{page.deliveryCitiesDescription.map((item) => (
+						<TableRow key={item.value}>
+							<TableCell sx={{ width: { xs: 'initial', md: '600px' } }}>
+								<Typography>{item.label}</Typography>
+							</TableCell>
+							<TableCell>
+								<Typography fontWeight='500'>{item.value}</Typography>
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
 
-            <Box>
-                <Typography marginTop="1em" textTransform="uppercase" withSeparator component="h2" variant="h4">
-                    {page.courierTitle}
-                </Typography>
-                <Table sx={{ marginY: '2em', maxWidth: 1030 }}>
-                    <TableBody>
-                        {page.courierDescription.map((item) => (
-                            <TableRow key={item.value}>
-                                <TableCell width={600}>
-                                    <Typography whiteSpace="nowrap">{item.label}</Typography>
-                                </TableCell>
-                                <TableCell>
-                                    <Typography fontWeight="500">{item.value}</Typography>
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </Box>
-            <Typography marginTop="1em" withSeparator component="h2" variant="h4" textTransform="uppercase">
-                {page.shipmentTitle}
-            </Typography>
-            <Box marginBottom="4em" display="flex" paddingTop="2em" alignItems="center">
-                <Box paddingRight="3em">
-                    <ReactMarkdown content={page.shipmentText}></ReactMarkdown>
-                </Box>
-                <Box>
-                    <Image
-                        title={page.shipmentImageRight?.caption}
-                        width={page.shipmentImageRight?.width}
-                        height={page.shipmentImageRight?.height}
-                        src={page.shipmentImageRight?.url}
-                        alt={page.shipmentImageRight?.alternativeText}></Image>
-                </Box>
-            </Box>
-            <BlockImages images={page.images2} sx={{ marginBottom: '-3em' }}></BlockImages>
-        </>
-    );
+			<Box>
+				<Typography marginTop='1em' textTransform='uppercase' withSeparator component='h2' variant='h4'>
+					{page.courierTitle}
+				</Typography>
+				<Table sx={{ marginY: '2em', maxWidth: 1030 }}>
+					<TableBody>
+						{page.courierDescription.map((item) => (
+							<TableRow key={item.value}>
+								<TableCell sx={{ width: { xs: 'initial', md: '600px' } }}>
+									<Typography>{item.label}</Typography>
+								</TableCell>
+								<TableCell>
+									<Typography fontWeight='500'>{item.value}</Typography>
+								</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</Box>
+			<Typography marginTop='1em' withSeparator component='h2' variant='h4' textTransform='uppercase'>
+				{page.shipmentTitle}
+			</Typography>
+			<Box
+				display='flex'
+				paddingTop='2em'
+				sx={{
+					flexDirection: { xs: 'column', md: 'row' },
+					alignItems: { xs: 'initial', md: 'center' },
+					marginBottom: { xs: '1em', md: '4em' },
+				}}
+			>
+				<Box sx={{ paddingRight: { xs: 0, md: '3em' }, marginBottom: { xs: '1em', md: 0 } }}>
+					<ReactMarkdown content={page.shipmentText}></ReactMarkdown>
+				</Box>
+				<Box>
+					<Image
+						title={page.shipmentImageRight?.caption}
+						width={page.shipmentImageRight?.width}
+						height={page.shipmentImageRight?.height}
+						src={page.shipmentImageRight?.url}
+						alt={page.shipmentImageRight?.alternativeText}
+					></Image>
+				</Box>
+			</Box>
+			<BlockImages
+				withoutOverlay={isTablet}
+				images={page.images2}
+				sx={{
+					marginBottom: '-3em',
+					flexDirection: { xs: 'column', md: 'row' },
+					marginTop: { xs: 0, md: '3em' },
+				}}
+			></BlockImages>
+		</>
+	);
 };
 
 export default Delivery;
 
 export const getStaticProps = getPageProps(
-    fetchPage('delivery', {
-        populate: [
-            'seo',
-            'images1',
-            'images2',
-            'mainImageLeft',
-            'deliveryCitiesDescription',
-            'courierDescription',
-            'shipmentImageRight'
-        ]
-    })
+	fetchPage('delivery', {
+		populate: [
+			'seo',
+			'images1',
+			'images2',
+			'mainImageLeft',
+			'deliveryCitiesDescription',
+			'courierDescription',
+			'shipmentImageRight',
+		],
+	})
 );
