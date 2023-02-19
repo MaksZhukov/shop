33060cd2c36ddbe052f099fa88e229fffbbe222b
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-02-19 11:05:55 +0300

    Added next/font lib for fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,16 @@
-import getConfig from 'next/config';
+import { roboto } from 'fonts';
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
     return (
         <Html lang="ru">
             <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
-                    rel="stylesheet"
-                />
                 <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png" />
                 <link rel="icon" type="image/png" sizes="96x96" href="/favicons/favicon-16x16.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
                 <link rel="manifest" href="/favicons/site.webmanifest" />
             </Head>
-            <body>
+            <body className={roboto.className}>
                 <Main />
                 <NextScript />
             </body>
