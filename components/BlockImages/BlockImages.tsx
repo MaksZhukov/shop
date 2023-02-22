46812a7402ee3bd2830358060da32acf6872b853
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-02-22 22:57:04 +0300

    Added sliders for guarantee page

--- components/BlockImages/BlockImages.tsx
+++ components/BlockImages/BlockImages.tsx
@@ -1,44 +1,62 @@
-import { Box, Container, SxProps } from '@mui/system';
+import { Box, SxProps } from '@mui/system';
 import { Image as IImage } from 'api/types';
 import Image from 'components/Image';
 import { FC } from 'react';
 import styles from './BlockImages.module.scss';
-import { useMediaQuery } from '@mui/material';
+import Slider from 'react-slick';
 
 interface Props {
-	images?: IImage[];
-	isOnSSR?: boolean;
-	withoutOverlay?: boolean;
-	sx?: SxProps<any>;
+    images?: IImage[];
+    isOnSSR?: boolean;
+    withSlider?: boolean;
+    withoutOverlay?: boolean;
+    sx?: SxProps<any>;
 }
 
-const BlockImages: FC<Props> = ({ images, withoutOverlay = false, sx }) => {
-	if (!images) {
-		return <></>;
-	}
-	return (
-		<Box
-			className={withoutOverlay ? '' : styles.overlay}
-			paddingY='3em'
-			marginY='3em'
-			display='flex'
-			gap={'1em'}
-			sx={sx}
-		>
-			{images.map((item) => (
-				<Box key={item.id}>
-					<Image
-						title={item.caption}
-						src={item.url}
-						alt={item.alternativeText}
-						width={390}
-						height={270}
-						style={{ height: 'auto' }}
-					></Image>
-				</Box>
-			))}
-		</Box>
-	);
+const BlockImages: FC<Props> = ({ images, withoutOverlay = false, withSlider = false, sx }) => {
+    if (!images) {
+        return <></>;
+    }
+    if (withSlider) {
+        return (
+            <Box paddingX="1em">
+                <Slider slidesToShow={1}>
+                    {images.map((item) => (
+                        <Box key={item.id} padding="0.5em">
+                            <Image
+                                title={item.caption}
+                                alt={item.alternativeText}
+                                width={390}
+                                height={270}
+                                style={{ height: 'auto' }}
+                                src={item.formats?.small?.url || item.url}></Image>
+                        </Box>
+                    ))}
+                </Slider>
+            </Box>
+        );
+    }
+    return (
+        <Box
+            className={withoutOverlay ? '' : styles.overlay}
+            paddingY="3em"
+            marginY="3em"
+            display="flex"
+            gap={'1em'}
+            sx={sx}>
+            {images.map((item) => (
+                <Box key={item.id}>
+                    <Image
+                        title={item.caption}
+                        src={item.url}
+                        alt={item.alternativeText}
+                        width={390}
+                        height={270}
+                        style={{ height: 'auto' }}></Image>
+                </Box>
+            ))}
+        </Box>
+    );
 };
 
 export default BlockImages;
