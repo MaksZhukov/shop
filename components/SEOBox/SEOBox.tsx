8871a880e0b0105c699b3e6eaf6fe293cdc9de47
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-02-22 16:10:26 +0300

    Added slider for col items for images for SEOBox block

--- components/SEOBox/SEOBox.tsx
+++ components/SEOBox/SEOBox.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Box, useMediaQuery } from '@mui/material';
 import { Image as IImage } from 'api/types';
 import Image from 'components/Image';
 import ReactMarkdown from 'components/ReactMarkdown';
@@ -6,6 +6,7 @@ import WhiteBox from 'components/WhiteBox';
 import getConfig from 'next/config';
 
 import { FC } from 'react';
+import Slider from 'react-slick';
 
 const { publicRuntimeConfig } = getConfig();
 
@@ -13,23 +14,43 @@ interface Props {
     images?: IImage[];
     content?: string;
 }
-
 const SEOBox: FC<Props> = ({ images, content }) => {
+    const isTablet = useMediaQuery((theme: any) => theme.breakpoints.down('md'));
     return (
         <>
             {images && (
-                <Box display="flex" flexWrap="wrap" justifyContent="space-around">
-                    {images?.map((item) => (
-                        <Box key={item.id} padding="0.5em">
-                            <Image
-                                title={item.caption}
-                                alt={item.alternativeText}
-                                width={208}
-                                height={156}
-                                src={item.formats?.small?.url || item.url}></Image>
+                <>
+                    {isTablet ? (
+                        <Box paddingX="1em">
+                            <Slider slidesToShow={2}>
+                                {images.map((item) => (
+                                    <Box key={item.id} padding="0.5em">
+                                        <Image
+                                            title={item.caption}
+                                            alt={item.alternativeText}
+                                            width={208}
+                                            height={156}
+                                            style={{ margin: 'auto', height: 'auto' }}
+                                            src={item.formats?.small?.url || item.url}></Image>
+                                    </Box>
+                                ))}
+                            </Slider>
                         </Box>
-                    ))}
-                </Box>
+                    ) : (
+                        <Box display="flex" flexWrap="wrap" justifyContent="space-around">
+                            {images?.map((item) => (
+                                <Box key={item.id} padding="0.5em">
+                                    <Image
+                                        title={item.caption}
+                                        alt={item.alternativeText}
+                                        width={208}
+                                        height={156}
+                                        src={item.formats?.small?.url || item.url}></Image>
+                                </Box>
+                            ))}
+                        </Box>
+                    )}
+                </>
             )}
             {content && <ReactMarkdown content={content}></ReactMarkdown>}
         </>
