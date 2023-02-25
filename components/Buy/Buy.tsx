286e0a2d17b476b1c6c4ebbf74b0149a04ec7069
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-02-25 22:47:21 +0300

    Added modal loader

--- components/Buy/Buy.tsx
+++ components/Buy/Buy.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 import Button from '@mui/material/Button';
-import { SxProps } from '@mui/material';
+import { Modal, SxProps } from '@mui/material';
 import { Product } from 'api/types';
 import { FC } from 'react';
 import { fetchOrderCheckout } from 'api/orders';
+import Loader from 'components/Loader';
 
 interface Props {
     sx?: SxProps;
@@ -12,14 +13,17 @@ interface Props {
 
 const Buy: FC<Props> = ({ sx, product }) => {
     const [token, setToken] = useState<string>('');
+    const [isLoadingToken, setIsLoadingToken] = useState<boolean>(false);
     const handleClickBuy = async () => {
         let newToken = token;
         if (!newToken) {
+            setIsLoadingToken(true);
             const {
                 data: { data }
             } = await fetchOrderCheckout(product.id, product.type);
             newToken = data.token;
             setToken(newToken);
+            setIsLoadingToken(false);
         }
         const params = {
             checkout_url: 'https://checkout.bepaid.by',
@@ -32,6 +36,9 @@ const Buy: FC<Props> = ({ sx, product }) => {
             <Button sx={sx} onClick={handleClickBuy} variant="contained">
                 Купить
             </Button>
+            <Modal open={isLoadingToken}>
+                <Loader></Loader>
+            </Modal>
         </>
     );
 };
