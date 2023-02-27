c4ecf3c517963a4b2d0144bafb7a37ed5e9eeaeb
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-02-27 23:06:48 +0300

    Added order

--- api/tires/types.ts
+++ api/tires/types.ts
@@ -1,3 +1,4 @@
+import { Order } from 'api/orders/types';
 import { TireBrand } from 'api/tireBrands/types';
 import { TireDiameter } from 'api/tireDiameters/types';
 import { TireHeight } from 'api/tireHeights/types';
@@ -7,23 +8,24 @@ import { Image, ProductSnippets, SEO } from 'api/types';
 export type Season = 'зимние' | 'летние' | 'всесезонные';
 
 export interface Tire {
-	id: number;
-	type: 'tire';
-	h1: string;
-	name: string;
-	slug: string;
-	diameter: TireDiameter;
-	width: TireWidth;
-	height: TireHeight;
-	season: Season;
-	brand: TireBrand;
-	price: number;
-	priceUSD: number;
-	discountPrice: number;
-	discountPriceUSD: number;
-	count: number;
-	description: string;
-	images: Image[];
-	seo?: SEO;
-	snippets?: ProductSnippets;
+    id: number;
+    type: 'tire';
+    h1: string;
+    name: string;
+    slug: string;
+    diameter: TireDiameter;
+    width: TireWidth;
+    height: TireHeight;
+    season: Season;
+    brand: TireBrand;
+    price: number;
+    priceUSD: number;
+    discountPrice: number;
+    discountPriceUSD: number;
+    count: number;
+    description: string;
+    images: Image[];
+    seo?: SEO;
+    snippets?: ProductSnippets;
+    order?: Order;
 }
