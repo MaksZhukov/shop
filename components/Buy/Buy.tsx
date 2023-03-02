ce8b930f10b5edf8e83db4c7d1a361565ca4783b
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-03-02 20:59:41 +0300

    Added successfull message

--- components/Buy/Buy.tsx
+++ components/Buy/Buy.tsx
@@ -5,48 +5,51 @@ import { Product } from 'api/types';
 import { FC } from 'react';
 import { fetchOrderCheckout } from 'api/orders';
 import Loader from 'components/Loader';
+import { useSnackbar } from 'notistack';
 
 interface Props {
-	sx?: SxProps;
-	product: Product;
-	onSold?: () => void;
+    sx?: SxProps;
+    product: Product;
+    onSold?: () => void;
 }
 
 const Buy: FC<Props> = ({ sx, product, onSold = () => {} }) => {
-	const [token, setToken] = useState<string>('');
-	const [isLoadingToken, setIsLoadingToken] = useState<boolean>(false);
-	const handleClickBuy = async () => {
-		let newToken = token;
-		if (!newToken) {
-			setIsLoadingToken(true);
-			const {
-				data: { data },
-			} = await fetchOrderCheckout(product.id, product.type);
-			newToken = data.token;
-			setToken(newToken);
-			setIsLoadingToken(false);
-		}
-		const params = {
-			checkout_url: 'https://checkout.bepaid.by',
-			token: newToken,
-			closeWidget: (status: string) => {
-				if (status === 'successful') {
-					onSold();
-				}
-			},
-		};
-		new BeGateway(params).createWidget();
-	};
-	return (
-		<>
-			<Button sx={sx} onClick={handleClickBuy} variant='contained'>
-				Купить
-			</Button>
-			<Modal open={isLoadingToken}>
-				<Loader></Loader>
-			</Modal>
-		</>
-	);
+    const [token, setToken] = useState<string>('');
+    const [isLoadingToken, setIsLoadingToken] = useState<boolean>(false);
+    const { enqueueSnackbar } = useSnackbar();
+    const handleClickBuy = async () => {
+        let newToken = token;
+        if (!newToken) {
+            setIsLoadingToken(true);
+            const {
+                data: { data }
+            } = await fetchOrderCheckout(product.id, product.type);
+            newToken = data.token;
+            setToken(newToken);
+            setIsLoadingToken(false);
+        }
+        const params = {
+            checkout_url: 'https://checkout.bepaid.by',
+            token: newToken,
+            closeWidget: (status: string) => {
+                if (status === 'successful') {
+                    onSold();
+                    enqueueSnackbar('Спасибо за заказ, вам прийдет уведомление на почту');
+                }
+            }
+        };
+        new BeGateway(params).createWidget();
+    };
+    return (
+        <>
+            <Button sx={sx} onClick={handleClickBuy} variant='contained'>
+                Купить
+            </Button>
+            <Modal open={isLoadingToken}>
+                <Loader></Loader>
+            </Modal>
+        </>
+    );
 };
 
 export default Buy;
