fd9c54df65fd52400f7845a53241131b5abf900a
Maksim Zhukau <maksimz@checkpoint.com>
2023-03-02 17:15:37 +0300

    Added onSold event

--- components/Buy/Buy.tsx
+++ components/Buy/Buy.tsx
@@ -7,40 +7,46 @@ import { fetchOrderCheckout } from 'api/orders';
 import Loader from 'components/Loader';
 
 interface Props {
-    sx?: SxProps;
-    product: Product;
+	sx?: SxProps;
+	product: Product;
+	onSold?: () => void;
 }
 
-const Buy: FC<Props> = ({ sx, product }) => {
-    const [token, setToken] = useState<string>('');
-    const [isLoadingToken, setIsLoadingToken] = useState<boolean>(false);
-    const handleClickBuy = async () => {
-        let newToken = token;
-        if (!newToken) {
-            setIsLoadingToken(true);
-            const {
-                data: { data }
-            } = await fetchOrderCheckout(product.id, product.type);
-            newToken = data.token;
-            setToken(newToken);
-            setIsLoadingToken(false);
-        }
-        const params = {
-            checkout_url: 'https://checkout.bepaid.by',
-            token: newToken
-        };
-        new BeGateway(params).createWidget();
-    };
-    return (
-        <>
-            <Button sx={sx} onClick={handleClickBuy} variant="contained">
-                Купить
-            </Button>
-            <Modal open={isLoadingToken}>
-                <Loader></Loader>
-            </Modal>
-        </>
-    );
+const Buy: FC<Props> = ({ sx, product, onSold = () => {} }) => {
+	const [token, setToken] = useState<string>('');
+	const [isLoadingToken, setIsLoadingToken] = useState<boolean>(false);
+	const handleClickBuy = async () => {
+		let newToken = token;
+		if (!newToken) {
+			setIsLoadingToken(true);
+			const {
+				data: { data },
+			} = await fetchOrderCheckout(product.id, product.type);
+			newToken = data.token;
+			setToken(newToken);
+			setIsLoadingToken(false);
+		}
+		const params = {
+			checkout_url: 'https://checkout.bepaid.by',
+			token: newToken,
+			closeWidget: (status: string) => {
+				if (status === 'successful') {
+					onSold();
+				}
+			},
+		};
+		new BeGateway(params).createWidget();
+	};
+	return (
+		<>
+			<Button sx={sx} onClick={handleClickBuy} variant='contained'>
+				Купить
+			</Button>
+			<Modal open={isLoadingToken}>
+				<Loader></Loader>
+			</Modal>
+		</>
+	);
 };
 
 export default Buy;
