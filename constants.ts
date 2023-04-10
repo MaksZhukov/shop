8b0765a0a91ace28782229924d80738ea5d8d58b
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-04-10 20:36:16 +0300

    Added category text

--- constants.ts
+++ constants.ts
@@ -1,22 +1,22 @@
-import { generateArrayOfYears } from "services/DateService";
+import { generateArrayOfYears } from 'services/DateService';
 
 export const PRIVATE_PATHS = ['/profile'];
 export const OFFSET_SCROLL_LOAD_MORE = 100;
 export const FUELS = ['бензин', 'дизель', 'гибрид', 'электро'] as const;
 export const TRANSMISSIONS = ['акпп', 'мкпп', 'робот', 'вариатор'] as const;
 export const BODY_STYLES = [
-	'седан',
-	'хэтчбек',
-	'универсал',
-	'внедорожник',
-	'минивэн',
-	'купе',
-	'лифтбек',
-	'пикап',
-	'кабриолет',
-	'фургон',
-	'бортовой',
-	'тягач',
+    'седан',
+    'хэтчбек',
+    'универсал',
+    'внедорожник',
+    'минивэн',
+    'купе',
+    'лифтбек',
+    'пикап',
+    'кабриолет',
+    'фургон',
+    'бортовой',
+    'тягач'
 ] as const;
 
 export const SEASONS = ['зимние', 'летние', 'всесезонные'] as const;
