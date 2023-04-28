3d316bedea1adef1bb6c9d9b392efb3c07e2f376
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-04-28 23:51:47 +0300

    Added seo for kindSpareParts

--- services/SEOService.ts
+++ services/SEOService.ts
@@ -1,10 +1,18 @@
 import { SEO } from 'api/types';
 
-export const withKindSparePart = (seo: SEO, kindSparePart?: string) => {
+export const withKindSparePart = (seo: SEO, appendAfter: string, kindSparePart?: string) => {
     if (kindSparePart) {
-        const arr = seo.h1.split(' ');
-        arr.splice(1, 0, kindSparePart);
-        return { ...seo, h1: arr.join(' ') };
+        const arrH1 = seo.h1.split(' ');
+        const arrTitle = seo.title.split(' ');
+        const arrDescription = seo.description.split(' ');
+        arrH1.splice(1, 0, kindSparePart);
+        arrTitle.splice(arrTitle.findIndex((item) => item.toLowerCase() === appendAfter) + 1, 0, kindSparePart);
+        arrDescription.splice(
+            arrDescription.findIndex((item) => item.toLowerCase() === appendAfter) + 1,
+            0,
+            kindSparePart
+        );
+        return { ...seo, title: arrTitle.join(' '), description: arrDescription.join(' '), h1: arrH1.join(' ') };
     }
     return seo;
 };
