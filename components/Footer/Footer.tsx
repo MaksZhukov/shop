b86b76b9ca6558da21cbd961da1c1d2987be146d
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-05-03 11:21:52 +0300

    Moved socials to contacts

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -32,82 +32,51 @@ const Footer: FC<Props> = ({ footer }) => {
     const title = typeof window !== 'undefined' ? document.title : '';
     return (
         <Box
-            component="footer"
-            bgcolor="secondary.main"
+            component='footer'
+            bgcolor='secondary.main'
             sx={{ padding: { xs: '20px 0 40px', md: '20px 0' } }}
             className={styles.footer}>
             <Container>
                 <Box
-                    display="flex"
+                    display='flex'
                     sx={{ flexWrap: { xs: 'wrap', md: 'nowrap' }, flexDirection: { xs: 'column', sm: 'row' } }}>
                     <Box
                         className={styles.footer__item}
                         sx={{
                             order: { xs: 4, sm: 1 },
-                            width: { md: '25%', xs: '100%' },
+                            width: { md: '33%', xs: '100%' },
                             paddingX: { xs: 0, md: '1.5em' }
                         }}>
                         <ReactMarkdown content={footer.firstBlock}></ReactMarkdown>
                         <NextLink href={'/privacy'}>
-                            <Link color="#fff" component="span">
+                            <Link color='#fff' component='span'>
                                 Политика конфиденциальности
                             </Link>
                         </NextLink>
                     </Box>
 
                     <Box
-                        display="flex"
-                        flexWrap="wrap"
-                        className={styles.footer__item}
-                        sx={{ order: { xs: 2 }, width: { md: '25%', xs: '100%' }, padding: { xs: 0, md: '0 1em' } }}>
-                        <Typography
-                            gutterBottom
-                            display={{ xs: 'block', md: 'none' }}
-                            width={'100%'}
-                            textTransform="uppercase">
-                            Мы в соц сетях:
-                        </Typography>
-                        {footer.socials?.map((item) => (
-                            <ListItemButton
-                                sx={{ padding: '0.5em 0.25em', width: { xs: 'auto', md: '50%' }, flex: 'initial' }}
-                                component="a"
-                                key={item.id}
-                                href={item.link}
-                                target="_blank"
-                                color="inherit">
-                                <Image
-                                    title={item.image?.caption}
-                                    alt={item.image?.alternativeText}
-                                    width={20}
-                                    height={20}
-                                    src={item.image?.url}></Image>
-                                <Typography marginLeft="0.5em">{item.image?.caption}</Typography>
-                            </ListItemButton>
-                        ))}
-                    </Box>
-
-                    <Box
                         className={(styles.footer__item, styles.footer__item_map)}
-                        sx={{ order: { xs: 3 }, width: { md: '25%', xs: '100%' }, paddingX: { xs: 0, md: '1em' } }}>
+                        sx={{ order: { xs: 3 }, width: { md: '33%', xs: '100%' }, paddingX: { xs: 0, md: '1em' } }}>
                         <iframe
-                            style={{ maxWidth: '100%' }}
-                            src="https://yandex.ru/map-widget/v1/?um=constructor%3Aa553e2f9544eb2f0c9143e3fc50b1dd10fc059188ae131165b0455a4ff8c645b&amp;source=constructor"
-                            frameBorder="0"></iframe>
+                            style={{ maxWidth: '100%', width: '100%' }}
+                            src='https://yandex.ru/map-widget/v1/?um=constructor%3Aa553e2f9544eb2f0c9143e3fc50b1dd10fc059188ae131165b0455a4ff8c645b&amp;source=constructor'
+                            frameBorder='0'></iframe>
                     </Box>
 
                     <Box
                         className={styles.footer__item}
                         sx={{
                             order: { xs: 1, md: 4 },
-                            width: { md: '25%', xs: '100%' },
+                            width: { md: '33%', xs: '100%' },
                             padding: { xs: 0, md: '0 1.5em 0 3em' }
                         }}>
                         <ReactMarkdown content={footer.fourthBlock}></ReactMarkdown>
-                        <Box marginBottom="0.5em">
+                        <Box marginBottom='0.5em'>
                             <Typography>Поделиться:</Typography>
                             <TwitterShareButton
                                 className={styles.share}
-                                url="http://twitter.com/share"
+                                url='http://twitter.com/share'
                                 title={title}
                                 via={'https://twitter.com/MZapcastej'}>
                                 <TwitterIcon size={25}></TwitterIcon>
@@ -118,14 +87,14 @@ const Footer: FC<Props> = ({ footer }) => {
                                 quote={title}>
                                 <FacebookIcon size={25}></FacebookIcon>
                             </FacebookShareButton>
-                            <VKShareButton className={styles.share} title={title} url="http://vk.com/share.php">
+                            <VKShareButton className={styles.share} title={title} url='http://vk.com/share.php'>
                                 <VKIcon size={25}></VKIcon>
                             </VKShareButton>
                             <PinterestShareButton
                                 className={styles.share}
                                 description={description}
                                 media={origin + '/logo.jpg'}
-                                url="http://www.pinterest.com/pin/create/button">
+                                url='http://www.pinterest.com/pin/create/button'>
                                 <PinterestIcon size={25}></PinterestIcon>
                             </PinterestShareButton>
                             <OKShareButton
@@ -138,14 +107,14 @@ const Footer: FC<Props> = ({ footer }) => {
                             </OKShareButton>
                             <LivejournalShareButton
                                 className={styles.share}
-                                url="http://www.livejournal.com/update.bml"
+                                url='http://www.livejournal.com/update.bml'
                                 title={title}
                                 description={description}>
                                 <LivejournalIcon size={25}></LivejournalIcon>
                             </LivejournalShareButton>
                         </Box>
                         <NextLink href={'/sitemap'}>
-                            <Link color="#fff" component="span">
+                            <Link color='#fff' component='span'>
                                 Карта сайта
                             </Link>
                         </NextLink>
@@ -153,15 +122,15 @@ const Footer: FC<Props> = ({ footer }) => {
                 </Box>
                 <Box overflow={{ xs: 'auto', md: 'initial' }}>
                     <Image
-                        title="Bepaid карточки"
-                        alt="Bepaid карточки"
+                        title='Bepaid карточки'
+                        alt='Bepaid карточки'
                         isOnSSR={false}
-                        src="/be_paid_cards.png"
+                        src='/be_paid_cards.png'
                         width={1452}
                         style={isTablet ? { maxWidth: 'initial' } : {}}
                         height={100}></Image>
                 </Box>
-                <Box color="#fff">
+                <Box color='#fff'>
                     <ReactMarkdown content={footer.textAfterPayments}></ReactMarkdown>
                 </Box>
             </Container>
