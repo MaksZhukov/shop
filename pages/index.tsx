60f5a4a4047294b63ec07c737fad80f2d385a609
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-05-24 11:05:30 +0300

    Removed unused code

--- pages/index.tsx
+++ pages/index.tsx
@@ -648,7 +648,6 @@ const Home: NextPage<Props> = ({ page, brands = [], reviews, articles }) => {
                     title={isMobile ? page.bannerMobile?.caption : page.banner?.caption}
                     width={isMobile ? page.bannerMobile?.width : page.banner?.width}
                     height={isMobile ? page.bannerMobile?.height : page.banner?.height}
-                    // loading='lazy'
                     style={{
                         position: 'absolute',
                         top: 0,
