ec6fa4e0745565ce0790bd25972b9dc2b767532a
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-06-01 00:59:11 +0300

    Fixed padding for brands slider

--- pages/cabins/[[...slug]].tsx
+++ pages/cabins/[[...slug]].tsx
@@ -31,7 +31,7 @@ interface Props {
 const Cabins: NextPage<Props> = ({ page, brands, data, relatedProducts, kindSparePart, setRenderBeforeFooter }) => {
     useEffect(() => {
         setRenderBeforeFooter(
-            <Box marginY='1em'>
+            <Box marginY='1em' paddingX="1em">
                 <BrandsSlider linkType='cabins' brands={brands}></BrandsSlider>
             </Box>
         );
