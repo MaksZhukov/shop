f35893149e432553687a3b288e0523891f819cd9
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-06-07 22:57:25 +0300

    Added tire brands for tires and tire

--- api/tireBrands/types.ts
+++ api/tireBrands/types.ts
@@ -1,9 +1,10 @@
-import { BrandTextComponent, SEO } from 'api/types';
+import { BrandTextComponent, Image, SEO } from 'api/types';
 
 export interface TireBrand {
 	id: number;
 	name: string;
 	slug: string;
+    image: Image;
 	seo: SEO;
 	productBrandText?: BrandTextComponent;
 }
--- pages/tires/[[...slug]].tsx
+++ pages/tires/[[...slug]].tsx
@@ -13,114 +13,150 @@ import Product from 'components/Product';
 import { Tire } from 'api/tires/types';
 import { AxiosError, AxiosHeaders } from 'axios';
 import { Brand } from 'api/brands/types';
+import { ReactElement, useEffect } from 'react';
+import BrandsSlider from 'components/BrandsSlider/BrandsSlider';
+import { Box } from '@mui/material';
 
 interface Props {
-    data?: Tire;
-    relatedProducts?: Tire[];
-    page: DefaultPage | (PageProduct & PageProductTire);
-    tireBrands: TireBrand[];
-    brands: Brand[];
+	data?: Tire;
+	relatedProducts?: Tire[];
+	page: DefaultPage | (PageProduct & PageProductTire);
+	tireBrands: TireBrand[];
+	brands: Brand[];
+	setRenderBeforeFooter: (element: ReactElement | null) => void;
 }
 
-const Tires: NextPage<Props> = ({ page, tireBrands, data, relatedProducts, brands }) => {
-    if (data && relatedProducts) {
-        return (
-            <Product
-                brands={brands}
-                data={data}
-                printOptions={[
-                    { text: 'Артикул', value: data.id },
-                    { text: 'Количество', value: data.count },
-                    { text: 'Марка', value: data.brand?.name },
-                    { text: 'Диаметр', value: data.diameter?.name },
-                    { text: 'Высота', value: data.height?.name },
-                    { text: 'Ширина', value: data.width?.name },
-                    { text: 'Сезон', value: data.season },
-                    { text: 'Описание', value: data.description }
-                ]}
-                page={page as PageProduct & PageProductTire}
-                relatedProducts={relatedProducts}></Product>
-        );
-    }
-    return <CatalogTires page={page} tireBrands={tireBrands}></CatalogTires>;
+const Tires: NextPage<Props> = ({ page, tireBrands, data, relatedProducts, brands, setRenderBeforeFooter }) => {
+	console.log(tireBrands);
+	useEffect(() => {
+		setRenderBeforeFooter(
+			<Box marginY='1em' paddingX='1em'>
+				<BrandsSlider linkType='tires' brands={tireBrands}></BrandsSlider>
+			</Box>
+		);
+		return () => {
+			setRenderBeforeFooter(null);
+		};
+	}, []);
+
+	if (data && relatedProducts) {
+		return (
+			<Product
+				brands={brands}
+				data={data}
+				printOptions={[
+					{ text: 'Артикул', value: data.id },
+					{ text: 'Количество', value: data.count },
+					{ text: 'Марка', value: data.brand?.name },
+					{ text: 'Диаметр', value: data.diameter?.name },
+					{ text: 'Высота', value: data.height?.name },
+					{ text: 'Ширина', value: data.width?.name },
+					{ text: 'Сезон', value: data.season },
+					{ text: 'Описание', value: data.description }
+				]}
+				page={page as PageProduct & PageProductTire}
+				relatedProducts={relatedProducts}></Product>
+		);
+	}
+	return <CatalogTires page={page} tireBrands={tireBrands}></CatalogTires>;
 };
 
 export default Tires;
 
 export const getServerSideProps = getPageProps(undefined, async (context) => {
-    const { slug } = context.query;
-    let tireBrands: TireBrand[] = [];
-    const brandParam = slug ? slug[0] : undefined;
-    const productSlugParam = slug ? slug[1] : undefined;
-    let seo: SEO | null = null;
-    let props: any = {};
-    if (productSlugParam) {
-        // if (brandParam.toLowerCase() === 'undefined') {
-        //     throw new AxiosError(undefined, undefined, undefined, undefined, {
-        //         statusText: '',
-        //         config: { headers: new AxiosHeaders() },
-        //         headers: {},
-        //         data: {},
-        //         status: 404
-        //     });
-        // } else {
-        const [
-            {
-                data: { data }
-            },
-            {
-                data: { data: page }
-            },
-            {
-                data: { data: pageTire }
-            }
-        ] = await Promise.all([
-            fetchTire(productSlugParam),
-            fetchPage<PageProduct>('product', { populate: ['whyWeBest.image'] })(),
-            fetchPage<PageProductTire>('product-tire', { populate: ['seo'] })()
-        ]);
-        const {
-            data: { data: relatedProducts }
-        } = await fetchTires({
-            filters: {
-                sold: { $eq: false },
-                id: {
-                    $ne: data.id
-                },
-                brand: data.brand?.id
-            },
-            populate: ['images', 'brand']
-        });
-        props = {
-            data,
-            page: {
-                ...page,
-                ...pageTire,
-                seo: getProductPageSeo(pageTire.seo, data)
-            },
-            relatedProducts
-        };
-        // }
-    } else if (brandParam) {
-        const [
-            {
-                data: { data }
-            },
-            {
-                data: { data: tireBrandsData }
-            }
-        ] = await Promise.all([
-            fetchTireBrandBySlug(brandParam, { populate: ['seo.images', 'image'] }),
-            fetchTireBrands({
-                pagination: { limit: API_MAX_LIMIT }
-            })
-        ]);
-        props = { page: { seo: data.seo }, tireBrands: tireBrandsData };
-    } else {
-        const {
-            data: { data }
-        } = await fetchPage('tire')();
-        props = { page: { seo: data.seo }, tireBrands: [] };
-    }
-    return props;
+	const { slug } = context.query;
+	let tireBrands: TireBrand[] = [];
+	const brandParam = slug ? slug[0] : undefined;
+	const productSlugParam = slug ? slug[1] : undefined;
+	let seo: SEO | null = null;
+	let props: any = {};
+	if (productSlugParam) {
+		// if (brandParam.toLowerCase() === 'undefined') {
+		//     throw new AxiosError(undefined, undefined, undefined, undefined, {
+		//         statusText: '',
+		//         config: { headers: new AxiosHeaders() },
+		//         headers: {},
+		//         data: {},
+		//         status: 404
+		//     });
+		// } else {
+		const [
+			{
+				data: { data }
+			},
+			{
+				data: { data: page }
+			},
+			{
+				data: { data: pageTire }
+			},
+			{
+				data: { data: tireBrandsData }
+			}
+		] = await Promise.all([
+			fetchTire(productSlugParam),
+			fetchPage<PageProduct>('product', { populate: ['whyWeBest.image'] })(),
+			fetchPage<PageProductTire>('product-tire', { populate: ['seo'] })(),
+			fetchTireBrands({
+				pagination: { limit: API_MAX_LIMIT },
+				populate: ['image']
+			})
+		]);
+		const {
+			data: { data: relatedProducts }
+		} = await fetchTires({
+			filters: {
+				sold: { $eq: false },
+				id: {
+					$ne: data.id
+				},
+				brand: data.brand?.id
+			},
+			populate: ['images', 'brand']
+		});
+		props = {
+			data,
+			page: {
+				...page,
+				...pageTire,
+				seo: getProductPageSeo(pageTire.seo, data)
+			},
+			tireBrands: tireBrandsData,
+			relatedProducts
+		};
+		// }
+	} else if (brandParam) {
+		const [
+			{
+				data: { data }
+			},
+			{
+				data: { data: tireBrandsData }
+			}
+		] = await Promise.all([
+			fetchTireBrandBySlug(brandParam, { populate: ['seo.images', 'image'] }),
+			fetchTireBrands({
+				pagination: { limit: API_MAX_LIMIT },
+				populate: ['image']
+			})
+		]);
+		props = { page: { seo: data.seo }, tireBrands: tireBrandsData };
+	} else {
+		const [
+			{
+				data: { data }
+			},
+			{
+				data: { data: tireBrandsData }
+			}
+		] = await Promise.all([
+			fetchPage('tire')(),
+			fetchTireBrands({
+				pagination: { limit: API_MAX_LIMIT },
+				populate: ['image']
+			})
+		]);
+		props = { page: { seo: data.seo }, tireBrands: [] };
+	}
+	return props;
 });
