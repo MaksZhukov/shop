91eea1cec2b639d42fe9b2826a64ac3df5252d19
MaksZhukov <maks_zhukov_97@mail.ru>
2023-06-22 23:17:32 +0300

    Revert "Added optimize for getting products"

--- pages/how-to-get-to.tsx
+++ pages/how-to-get-to.tsx
@@ -1,31 +1,21 @@
-import { Box, Typography, useMediaQuery } from '@mui/material';
+import { Typography } from '@mui/material';
 import { fetchPage } from 'api/pages';
 import { DefaultPage } from 'api/pages/types';
-import { Image } from 'api/types';
-import dynamic from 'next/dynamic';
+import ReactMarkdown from 'components/ReactMarkdown';
 import { FC } from 'react';
 import { getPageProps } from 'services/PagePropsService';
 
-const ReactPlayer = dynamic(() => import('react-player'), { ssr: false });
-
 interface Props {
-	page: DefaultPage & { map: string; text: string; video: Image };
+	page: DefaultPage & { content: string };
 }
 
 const HowToGetTo: FC<Props> = ({ page }) => {
-	const isMobile = useMediaQuery((theme: any) => theme.breakpoints.down('sm'));
 	return (
 		<>
 			<Typography marginBottom='1em' component='h1' textTransform='uppercase' variant='h4' textAlign='center'>
 				{page.seo?.h1}
 			</Typography>
-			<Box display='flex' gap='3em' marginBottom='2em' sx={{ flexWrap: { xs: 'wrap', md: 'nowrap' } }}>
-				<Box flex='1'>{/* <ReactMarkdown content={page.text}></ReactMarkdown> */}</Box>
-				<Box width={{ xs: '100%', sm: 400 }}>
-					{/* <ReactPlayer width={isMobile ? '100%' : 350} height={400} url={page.video.url}></ReactPlayer> */}
-				</Box>
-			</Box>
-			{/* <Box dangerouslySetInnerHTML={{ __html: page.map }}></Box> */}
+			<ReactMarkdown content={page.content}></ReactMarkdown>
 		</>
 	);
 };
