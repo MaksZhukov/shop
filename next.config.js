fdf19bf76752bb75291e5f965c87e2e1d4bc1a85
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-12-10 11:46:18 +0300

    Added cache images in nextjs

--- next.config.js
+++ next.config.js
@@ -3,19 +3,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 
 const nextConfig = {
-	headers: async () => [
-		{
-			// list more extensions here if needed; these are all the resources in the `public` folder including the subfolders
-			source: '/:all*(svg|jpg|png|webp)',
-			locale: false,
-			headers: [
-				{
-					key: 'Cache-Control',
-					value: 'public, max-age=31536000, stale-while-revalidate'
-				}
-			]
-		}
-	],
 	productionBrowserSourceMaps: true,
 	reactStrictMode: false,
 	...(process.env.PAGES ? { pageExtensions: ['dev.tsx'] } : {}),
@@ -31,7 +18,8 @@ const nextConfig = {
 				protocol: 'https',
 				hostname: '**'
 			}
-		]
+		],
+		minimumCacheTTL: 60000
 	},
 	experimental: {
 		largePageDataBytes: 200 * 1000,
