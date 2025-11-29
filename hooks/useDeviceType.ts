a536ac6c1222a39a7408af35f53b5cadd607dad9
Maks Zhukov <maks_zhukov_97@mail.ru>
2025-11-29 09:43:03 +0100

    Fixed device type calc

--- hooks/useDeviceType.ts
+++ hooks/useDeviceType.ts
@@ -1,16 +1,14 @@
-import { useState, useEffect } from 'react';
+import { useMemo } from 'react';
 import { UAParser } from 'ua-parser-js';
 
 export type DeviceType = 'desktop' | 'mobile';
 
 export const useDeviceType = (): DeviceType => {
-	const [deviceType, setDeviceType] = useState<DeviceType>('desktop');
-
-	useEffect(() => {
+	const deviceType = useMemo(() => {
 		const parser = new UAParser();
 		const device = parser.getDevice();
 		const deviceTypeResult = device.type === 'mobile' || device.type === 'tablet' ? 'mobile' : 'desktop';
-		setDeviceType(deviceTypeResult);
+		return deviceTypeResult;
 	}, []);
 
 	return deviceType;
